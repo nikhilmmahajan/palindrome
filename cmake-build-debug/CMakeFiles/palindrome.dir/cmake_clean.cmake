802154abda55b39079c299352a7ef1e66593932b
nikhilmmahajan <nikhil.mm@gmail.com>
2018-02-19 12:09:03 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/palindrome.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/palindrome.dir/main.cpp.obj"
+  "palindrome.pdb"
+  "palindrome.exe"
+  "palindrome.exe.manifest"
+  "libpalindrome.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/palindrome.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+bool isPalin(const char *s, int first, int last)
+{
+    if (first == last  ) return true;
+    if( first > last ) return true;
+
+    return (s[first] == s[last] ? isPalin(s,first+1, last-1): false);
+
+}
+
+int main() {
+
+    int counter = 1;
+    string s;
+    while(counter) {
+        cout << "Enter your string for palindrome test" << std::endl;
+        getline(cin, s);
+
+        cout<<"\nOriginal string is :"<<s;
+        s.erase(remove(s.begin(),s.end(),' '),s.end());
+        cout<<"\nFinal string being checked:"<<s;
+
+        cout <<"\n1 is true, 0 is false : "<<s<<": "<< isPalin(s.c_str(),0,s.size()-1);
+
+        cout<< "\n\n\nEnter 1 to try again or 0\n";
+        cin>>counter;
+    }
+    return 0;
+}
+
